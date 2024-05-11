040538fe474dee1dc0be1a8f0ace41e0f82c7576
Carl Antoine (Ant Designs LLC) <carlantoine14@gmail.com>
2024-05-11 11:27:54 -0400

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,6 @@ import { Typography } from "@material-tailwind/react";
 export function Footer() {
   return (
     <footer className="w-full bg-white p-8">
-      <hr className="my-8 border-blue-gray-50" />
       <div className="flex flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 bg-white text-center md:justify-between">
         <img src="/logo.png" alt="logo-ct" className="w-30" />
         <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
