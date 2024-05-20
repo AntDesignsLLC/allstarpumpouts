a6c129edf8f62406c1d32f1e2c45efc4b9fb36a3
Carl Antoine (Ant Designs LLC) <carlantoine14@gmail.com>
2024-05-20 11:26:19 -0400

    google manager

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,6 +12,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-11141175218"></Script>
+      <Script>
+        {
+          `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date()); gtag('config', 'AW-11141175218');
+          `
+        }
+      </Script>
       <body className={inter.className}>{children}</body>
     </html>
   );
