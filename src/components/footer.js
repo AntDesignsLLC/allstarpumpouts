ffc654be43e6886e607d47925b23b1363e654f0c
Carl Antoine (Ant Designs LLC) <carlantoine14@gmail.com>
2025-05-29 22:38:19 -0400

    new logo

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <footer className="w-full bg-white p-8">
       <div className="flex flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 bg-white text-center md:justify-between">
-        <img src="/logo.png" alt="logo-ct" className="w-30" />
+        <img src="/logo_new.png" width={270} alt="logo-ct" className="w-30" />
         <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
           <li>
             <Typography
